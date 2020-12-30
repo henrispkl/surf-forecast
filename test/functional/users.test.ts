8f1902fe23c1e680d993f35160bbc2a34ff5373c
henrispkl <henriqueirineu10@hotmail.com>
2020-12-30 17:00:02 -0300

    User Module - Json Web Token with Node

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -33,7 +33,6 @@ describe('Users functional test', () => {
       };
 
       const response = await global.testRequest.post('/users').send(newUser);
-
       expect(response.status).toBe(422);
       expect(response.body).toEqual({
         code: 422,
@@ -56,6 +55,49 @@ describe('Users functional test', () => {
         code: 409,
         error: 'User validation failed: email: already exists in the database.',
       });
+    });
+  });
+
+  describe('When authenticating an user', () => {
+    it('should generate a token for a valid user', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: newUser.password });
+
+      expect(response.body).toEqual(
+        expect.objectContaining({ token: expect.any(String) })
+      );
+    });
+
+    it('Should return UNAUTHORIZED if the user with the given email is not found', async () => {
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: 'some-email@mail.com', password: '1234' });
+
+      expect(response.status).toBe(401);
+    });
+
+    it('Should return UNAUTHORIZED if the user is found but the password does not match', async () => {
+      const newUser = {
+        name: 'John Doe',
+        email: 'john@email.com',
+        password: '1234',
+      };
+
+      await new User(newUser).save();
+      const response = await global.testRequest
+        .post('/users/authenticate')
+        .send({ email: newUser.email, password: 'different password' });
+
+      expect(response.status).toBe(401);
     });
   });
 });
