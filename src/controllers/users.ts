b52460c631845b919c10eceef6eee15fc925b788
henrispkl <henriqueirineu10@hotmail.com>
2021-01-25 17:58:02 -0300

    Rating module - endpoint for token decoding

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,8 +1,9 @@
-import { Controller, Post } from '@overnightjs/core';
+import { Controller, Get, Middleware, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from '.';
 import AuthService from '@src/services/auth';
+import { authMiddleware } from '@src/middleware/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -40,5 +41,23 @@ export class UsersController extends BaseController {
 
     const token = AuthService.generateToken(user.toJSON());
     return res.status(200).send({ ...user.toJSON(), token });
+  }
+
+  @Get('me')
+  @Middleware(authMiddleware)
+  public async me(req: Request, res: Response): Promise<Response> {
+    // eslint-disable-next-line
+    // @ts-ignore
+    const email = req.decoded ? req.decoded.email : undefined;
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return this.sendErrorResponse(res, {
+        code: 404,
+        message: 'User not found!',
+      });
+    }
+
+    return res.send({ user });
   }
 }
